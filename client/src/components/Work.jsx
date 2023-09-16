052552b0f848e9ceb4f15e84fba0f86db1be14a9
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-09-16 14:46:06 +0530

    Done with Work

--- client/src/components/Work.jsx
+++ client/src/components/Work.jsx
@@ -1,12 +1,17 @@
-import React from "react";
-import Lottie from "lottie-react";
+import Lottie from 'lottie-react'
+import React from 'react'
 import HistoryAnimation from "../img/history-animation.json";
-import HeroAnimation from "../img/hero-animation.json";
+import PeopleAnimation from "../img/people-animation.json";
 
-const HeroComponent = () => {
+
+const Work = () => {
   return (
-    <div>
+
       <section className="grid grid-cols-1 md:grid-cols-2 w-full" id="home">
+      <div className="flex-1 flex items-center relative md:h-225  md:mt-0">
+          {/* Image */}
+          <Lottie animationData={PeopleAnimation} />
+        </div>
         <div className="flex-1 flex flex-col md:items-start items-center justify-center md:h-[75%] gap-6">
           <p className="text-[2.5rem] text-center md:text-left lg:text-[3.5rem] font-bold tracking-wide md:leading-tight">
             <span className="text-lightPrimary text-[3rem] lg:text-[4rem] mb-12">
@@ -15,7 +20,7 @@ const HeroComponent = () => {
             dao
           </p>
 
-          <p className="text-center md:text-justify text-lighttextGray text-sm flex flex-wrap gap-1">
+          <p className="text-center md:text-justify text-lighttextGray md:h-[85%] text-sm flex flex-wrap gap-1">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
@@ -24,13 +29,10 @@ const HeroComponent = () => {
             remaining essentially...
           </p>
         </div>
-        <div className="flex-1 flex items-center relative md:h-[75%]  md:mt-0">
-          {/* Image */}
-          <Lottie animationData={HeroAnimation} />
-        </div>
+        
       </section>
-    </div>
-  );
-};
+  
+  )
+}
 
-export default HeroComponent;
+export default Work
--- client/src/pages/TFL.jsx
+++ client/src/pages/TFL.jsx
@@ -1,35 +1,13 @@
 import React from 'react';
+import TFLcards from '../components/TFLcards';
 
 const TFL = () => {
   return (
-    <section
-        className="grid grid-cols-1 md:grid-cols-2 w-full"
-        id="home"
-      >
-    <div className='h-screen bg-darkBg text-lightModeTextColor'>
-      <div className="container mx-auto py-8">
-        <div className="flex space-x-4">
-          {/* Card 1 */}
-          <div className="flex-shrink-0 bg-white rounded-lg shadow-md p-4">
-            <h2 className="text-xl font-semibold mb-2">Card 1</h2>
-            <p className="text-gray-600">Total Users</p>
-          </div>
-
-          {/* Card 2 */}
-          <div className="flex-shrink-0 bg-white rounded-lg shadow-md p-4">
-            <h2 className="text-xl font-semibold mb-2">Card 2</h2>
-            <p className="text-gray-600">Total Funds L</p>
-          </div>
-
-          {/* Card 3 */}
-          <div className="flex-shrink-0 bg-white rounded-lg shadow-md p-4">
-            <h2 className="text-xl font-semibold mb-2">Card 3</h2>
-            <p className="text-gray-600">Total People Secured</p> 
-          </div>
-        </div>
-      </div>
+    <div className='conatai'>
+     
+    <TFLcards />
+  
     </div>
-    </section>
   );
 }
 
