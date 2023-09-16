3882f11a9a5da6e2532cad7e832024473839a15e
swayam karle <swayamkarle2@gmail.com>
2023-09-16 20:51:55 +0530

    Swayam's commit

--- client/src/App.js
+++ client/src/App.js
@@ -9,20 +9,63 @@ import Token from "./pages/Token";
 import ContactUs from "./pages/ContactUs";
 import SecureYourself from "./pages/SecureYourself";
 import Footer from "./components/Footer";
+import { useState, useEffect } from "react";
+const { ethers } = require("ethers");
+const {
+  MyTokenABI,
+  MyTokenByteCode,
+  MyTokenContractAddress,
+  ReliefDaoABI,
+  ReliefDaoByteCode,
+  ReliefDaoContractAddress,
+} = require("./contract_instances/config");
 
 function App() {
+  const [account, setAccount] = useState("");
+  const [contractMyToken, setContractMyToken] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [contractRelief, setContractRelief] = useState(null);
+  useEffect(() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const loadProvider = async () => {
+      if (provider) {
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        const contract1 = new ethers.Contract(
+          MyTokenContractAddress,
+          MyTokenABI,
+          signer
+        );
+        const contract2 = new ethers.Contract(
+          ReliefDaoContractAddress,
+          ReliefDaoABI,
+          signer
+        );
+        setContractMyToken(contract1);
+        setContractRelief(contract2);
+        setProvider(provider);
+        console.log(contract1.address);
+      } else {
+        alert("Wallet is not present");
+      }
+    };
+    provider && loadProvider();
+  }, []);
+
   return (
     <div>
       <Header />
       <div className=" p-3 px-4 md:p-6 md:px-16 b">
         <Routes>
           <Route path="/*" element={<HomePage />} />
-          <Route path="/secure-yourself" element={<SecureYourself />} />
+          <Route path="/secure-yourself" element={<SecureYourself account={account} provider={provider} contractMyToken={contractMyToken} contractRelief={contractRelief}/>} />
           <Route path="/tfl" element={<TFL />} />
           <Route path="/token" element={<Token />} />
           <Route path="/contact-us" element={<ContactUs />} />
           <Route path="/connect-wallet" element={<ConnectWallet />} />
-        </Routes> 
+        </Routes>
       </div>
       <Footer />
     </div>
