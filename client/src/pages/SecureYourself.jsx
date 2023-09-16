97a36c19276d9fb720ba9741903c7de1dc2a10eb
swayam karle <swayamkarle2@gmail.com>
2023-09-17 04:11:47 +0530

    Cool Commit

--- client/src/pages/SecureYourself.jsx
+++ client/src/pages/SecureYourself.jsx
@@ -17,7 +17,17 @@ const SecureYourself = ({
   const [nameToken, setNameToken] = useState("");
   const [symbolToken, setSymbolToken] = useState("");
   const [trustAddress, setTrustAddress] = useState();
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
+  const [amount, setAmount] = useState('');
+  const [recipient, setRecipient] = useState('');
+
+  const handleAmountChange = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const handleRecipientChange = (event) => {
+    setRecipient(event.target.value);
+  };
   useEffect(() => {
     async function fetchBalance() {
       try {
@@ -41,26 +51,33 @@ const SecureYourself = ({
     }
   }, [signer]);
 
-  const permit = async()=>{
-    
-  }
+  const permit = async () => {};
 
-  const yourTrustWorthyParty=async()=>{
+  const yourTrustWorthyParty = async () => {
     console.log(signer.getAddress());
     const address = await contractMyToken.brother(signer.getAddress());
     setTrustAddress(address);
-  }
-  const addTrustWorthy=async(event)=>{
+  };
+  const addTrustWorthy = async (event) => {
     event.preventDefault();
     await contractMyToken.addbrother(inputValue);
-  }
+  };
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
-  return (
+  const handleTransfer = async(event) => {
+    event.preventDefault();
+    // You can perform actions with the amount and recipient here, e.g., submit a transaction.
+    const parsedAmount = ethers.utils.parseEther(amount);
+    await contractMyToken.transfer(recipient,parsedAmount);
+    // Reset the form
+    setAmount('');
+    setRecipient('');
+  };
 
+  return (
     <div className="md:pb-36 h-screen flex mt-16 justify-center w-full text-lightModeTextColor">
       <div className=" md:w-1/2 w-[85%] rounded-lg h-1/2 border md:p-16 p-4 flex flex-col items-start justify-center">
         <p className=" md:text-xl text-lg">
@@ -87,28 +104,56 @@ const SecureYourself = ({
         ) : (
           <p className="font-semibold text-lightPrimary">Loading balance...</p>
         )}
-      </div>{
-        trustAddress && <p>Your trust Address is : {trustAddress}</p>
-
-      }
-      {!trustAddress &&
-       <div>
-      <h2>Input Form</h2>
-      <form onSubmit={addTrustWorthy}>
-        <label>
-          Enter something:
+      </div>
+      {trustAddress && <p>Your trust Address is : {trustAddress}</p>}
+      {!trustAddress && (
+        <div>
+          <h2>Input Form</h2>
+          <form onSubmit={addTrustWorthy}>
+            <label>
+              Enter something:
+              <input
+                className="text-black"
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder="Type something..."
+              />
+            </label>
+            <button type="submit">Add Trust Worthy</button>
+          </form>
+        </div>
+      )}
+          <div className="card">
+      <h2>Transfer Funds</h2>
+      <form onSubmit={handleTransfer}>
+        <div className="form-group">
+          <label htmlFor="amount">Amount:</label>
           <input
           className="text-black"
             type="text"
-            value={inputValue}
-            onChange={handleInputChange}
-            placeholder="Type something..."
+            id="amount"
+            value={amount}
+            onChange={handleAmountChange}
+            placeholder="Enter amount"
+            required
           />
-        </label>
-        <button type="submit">Add Trust Worthy</button>
+        </div>
+        <div className="form-group">
+          <label htmlFor="recipient">Recipient:</label>
+          <input
+          className="text-black"
+            type="text"
+            id="recipient"
+            value={recipient}
+            onChange={handleRecipientChange}
+            placeholder="Enter recipient's address"
+            required
+          />
+        </div>
+        <button type="submit">Transfer</button>
       </form>
     </div>
-}
     </div>
   );
 };
