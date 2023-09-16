c86ee28b173cf46d7286b16be510b9117f1a5c96
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-09-16 16:19:39 +0530

    Changing Header Activity

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,3 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
@@ -48,7 +47,7 @@ module.exports = {
         "2xl": "1536px",
       },
       lineHeight: {
-        tight: '1.2', // Adjust the value as needed
+        tight: "1.2", // Adjust the value as needed
       },
       colors: {
         lightModeHeadingheadingColor: "#000000",
@@ -56,10 +55,10 @@ module.exports = {
         lighttextGray: "#929292",
         lightPrimary: "#FFFC10",
         lightCard: "#FFFFFF",
-        footerSubtitle: 'rgba(255, 255, 255, 0.55)',
-        lightBgBlue: 'rgba(13, 27, 207, 0.05)',
-        yellow: '#FFFC10',
-        darkBg: '#1d1d1d'
+        footerSubtitle: "rgba(255, 255, 255, 0.55)",
+        lightBgBlue: "rgba(13, 27, 207, 0.05)",
+        yellow: "#FFFC10",
+        darkBg: "#1d1d1d",
       },
     },
   },
